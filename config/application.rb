0f3fea6e482d06618d0014ecd3779149dd75a6f3
Ben Gruber <blgruber@gmail.com>
2013-09-04 17:26:42 -0400

    first rounds of styling, getting to nav and profile page

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,8 @@ module Rr
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts" #added fonts folder
+
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible

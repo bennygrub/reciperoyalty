e63e7992ef3d623ef84f806acc6de16e2af5842c
Ben Gruber <blgruber@gmail.com>
2013-09-05 17:54:34 -0400

    styling homepage including new application layout for full screen

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem "paperclip", "~> 3.0"
 gem "nested_form"
 gem 'acts-as-taggable-on'
 gem 'acts_as_commentable', '3.0.1'
+gem 'flexslider'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,13 @@
 class StaticController < ApplicationController
+  layout "fullpage", :only => [:home, :about]
   def home
+  	@title = "Recipe Royalty | There Can Be Only One BEST Recipe"
+  	@featured = Recipe.where("slider = ?", true)
   end
 
   def about
+  	@title = "Recipe Royalty | There Can Be Only One BEST Recipe"
+  	@featured = Recipe.where("slider = ?", true)
   end
 
   def contact

53d9b571105fc92af3d98916cf8d5be0f5ad0ad0
Ben Gruber <blgruber@gmail.com>
2013-09-17 18:36:01 -0400

    fixed bugs for spacing on directions and video empty string

--- app/mailers/recipe_mailer.rb
+++ app/mailers/recipe_mailer.rb
@@ -1,12 +1,12 @@
 class RecipeMailer < ActionMailer::Base
-  default from: "ben@reciperoyalty.com"
+  default from: "Recipe Royalty <ben@reciperoyalty.com>"
 
   def create_recipe(user, recipe, winner, dish)
     @user = user
     @recipe = recipe
     @winner = winner
     @dish = dish
-    mail(to: @user.email, subject: "Congratulations - You created a recipe challenge for #{@dish}")
+    mail(to: @user.email, subject: "Congratulations - You created a recipe challenge for #{@dish.name}")
   end
 
 end

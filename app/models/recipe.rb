5e5b6ff77f2ce417103b5b6050217ae3cad84d78
Ben Gruber <blgruber@gmail.com>
2013-09-26 13:23:55 -0400

    change recipe name character limit to 40

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,7 +7,7 @@ class Recipe < ActiveRecord::Base
   accepts_nested_attributes_for :recipe_images, reject_if: :all_blank, :allow_destroy => true#, :reject_if => lambda { |t| t[:recipe_image].blank? }
   validates :cook, :difficulty, :directions, :ingredients, :introduction, :name, :prep, :serving, :dish_id, :presence => true
   validate :require_photo
-  validates_length_of :name, :maximum => 28
+  validates_length_of :name, :maximum => 40
   acts_as_commentable
   has_many :comments, :as => :commentable, :dependent => :destroy
 
